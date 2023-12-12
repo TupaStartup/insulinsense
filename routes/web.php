fda31c1f6ffdba9ac7a93df35fc613623510e721
Gilton Caminha <gilton.caminha@itec.ufpa.br>
2023-12-12 20:22:22 +0000

    front funcionando

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use resources\css;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::get('/perfil', function () {
+    return view('perfil');
 });
