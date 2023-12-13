90f978d9dca0ae5523c3d67fb6f94776ce99d698
Wesley <ec2-user@ip-172-31-80-152.ec2.internal>
2023-12-13 19:57:35 +0000

    modificações wesley

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,23 @@ Route::get('/', function () {
 Route::get('/perfil', function () {
     return view('perfil');
 });
+
+Route::get('/consultas', function () {
+    return view('consultas');
+});
+
+Route::get('/pacientes', function () {
+    return view('pacientes');
+});
+
+Route::get('/novaconsulta', function () {
+    return view('novaconsulta');
+});
+
+Route::get('/cadastro', function () {
+    return view('cadastro');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
